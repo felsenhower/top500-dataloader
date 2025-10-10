16c0ff7362d88c22d815014e4620cdc929da4d98
Ruben Felgenhauer <Ruben.Felgenhauer@posteo.de>
2025-10-11 00:10:22 +0200

    Move main() to __init__.py

--- src/top500/__init__.py
+++ src/top500/__init__.py
@@ -2,7 +2,9 @@
 top500. A TOP500 list downloader and dataloader for polars.
 """
 
+import argparse
 import csv
+import dataclasses
 import os
 import re
 import shutil
@@ -19,13 +21,12 @@ from urllib.parse import urljoin
 import pandas as pd
 import platformdirs
 import polars as pl
+import pydantic
 import requests
 from bs4 import BeautifulSoup
 from bs4.element import Tag as HtmlTag
 from pydantic import HttpUrl, TypeAdapter
-import pydantic
 from ratelimit import limits, sleep_and_retry
-import dataclasses
 
 
 @pydantic.dataclasses.dataclass
@@ -616,3 +617,64 @@ def read_list(
     assert filename.exists()
     with tarfile.open(filename, "r:gz") as tar:
         return readers[source](tar)
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser(
+        prog="top500", description="Download or view TOP500 lists."
+    )
+    parser.add_argument(
+        "-d",
+        "--download-dir",
+        action="store",
+        metavar="dir",
+        help=f'Set the download dir. Defaults to "{_DEFAULT_DOWNLOAD_DIR}".',
+    )
+    subparsers = parser.add_subparsers(dest="action", required=True)
+    subparsers.add_parser(
+        "list-online", help="List TOP500 list issues that are available online."
+    )
+    subparsers.add_parser(
+        "list-local", help="List TOP500 list issues that are available locally."
+    )
+    download_parser = subparsers.add_parser(
+        "download",
+        help='Download a TOP500 list issue (see "download --help" for more info).',
+    )
+    download_parser.add_argument("key", help='The key of the list, e.g. "2025-06".')
+    subparsers.add_parser(
+        "download-all",
+        help="Download all TOP500 list issues that are available online.",
+    )
+    display_parser = subparsers.add_parser(
+        "display",
+        help='Display a TOP500 list on the console (see "display --help" for more info).',
+    )
+    display_parser.add_argument("key", help='The key of the list, e.g. "2025-06".')
+    args = parser.parse_args()
+    if args.download_dir:
+        set_download_dir(args.download_dir)
+
+    def display_list_list(lists: Iterable[Top500ListInfo]) -> None:
+        with pl.Config(tbl_rows=-1, fmt_str_lengths=1000):
+            df = pl.DataFrame(lists)
+            if len(df) == 0:
+                print("No entries!")
+                return
+            print(df)
+
+    match args.action:
+        case "list-online":
+            display_list_list(iter_lists_online())
+        case "list-local":
+            display_list_list(iter_lists_local())
+        case "download":
+            download_list(args.key)
+        case "download-all":
+            download_all_lists()
+        case "display":
+            df = read_list(args.key, allow_download=True, source="normalized-pretty")
+            with pl.Config(tbl_rows=-1):
+                print(df)
+        case _:
+            raise RuntimeError(f'Encountered an unexpected argument "{args.action}"')
