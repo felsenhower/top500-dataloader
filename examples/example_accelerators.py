3828cd633eac8bf1965c280f43e8487cfaccccd8
Ruben Felgenhauer <Ruben.Felgenhauer@posteo.de>
2025-10-11 00:36:32 +0200

    More example

--- examples/example_accelerators.py
+++ examples/example_accelerators.py
@@ -10,19 +10,15 @@ import top500
 
 def main():
     has_acc_by_list = {}
-    for list_info in top500.iter_lists_local():
+    for list_info in top500.iter_lists_local(newest_first=False):
         df = top500.read_list(list_info, allow_download=False, source="normalized")
         assert len(df) == 500
         has_acc = list(df["accelerator"].is_null().not_())
         if sum(has_acc) > 0:
             has_acc_by_list[list_info.key] = has_acc
-    x = list(reversed(list(has_acc_by_list.keys())))
+    x = list(has_acc_by_list.keys())
     for limit in (500, 250, 50):
-        y = list(
-            reversed(
-                [(100.0 * sum(l[:limit]) / limit) for l in has_acc_by_list.values()]
-            )
-        )
+        y = [(100.0 * sum(l[:limit]) / limit) for l in has_acc_by_list.values()]
         plt.plot(x, y, label=f"TOP {limit}")
         print(list(zip(x, y)), limit)
     plt.xticks(rotation=90)
---
+++ examples/example_rmax.py
@@ -0,0 +1,29 @@
+"""
+This example plots the Rmax of the #1 TOP500 system over time.
+
+Download the lists before you run it.
+"""
+
+import matplotlib.pyplot as plt
+import top500
+
+
+def main():
+    x = []
+    y = []
+    for list_info in top500.iter_lists_local(newest_first=False):
+        x.append(list_info.key)
+        df = top500.read_list(list_info, allow_download=False, source="normalized")
+        rmax = df["r-max-gflops"][0]
+        y.append(rmax)
+    plt.plot(x, y)
+    plt.xticks(rotation=90)
+    plt.xlabel("TOP500 list release")
+    plt.ylabel("Rmax [FLOP/s]")
+    plt.yscale("log")
+    plt.tight_layout()
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
